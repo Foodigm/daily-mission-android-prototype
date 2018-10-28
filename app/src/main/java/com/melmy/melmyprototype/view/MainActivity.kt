bcda33e3f31fad5febfa5b51b185c216b91ff838
Sung Ho Hwang <sgc109109@gmail.com>
2018-10-29 02:47:38 +0900

    DB 디버깅을 위한 버튼 추가

--- app/src/main/java/com/melmy/melmyprototype/view/MainActivity.kt
+++ app/src/main/java/com/melmy/melmyprototype/view/MainActivity.kt
@@ -3,6 +3,15 @@ package com.melmy.melmyprototype.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.melmy.melmyprototype.R
+import com.melmy.melmyprototype.db.AppDatabase
+import com.melmy.melmyprototype.model.CompletedHistory
+import com.melmy.melmyprototype.model.Mission
+import com.melmy.melmyprototype.model.MissionType
+import com.melmy.melmyprototype.utils.DayOfWeekSet
+import java.util.*
+import 	androidx.sqlite.db.transaction
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +19,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val db = AppDatabase.getInstance(this)
+        val mission = Mission(
+                0,
+                "test",
+                false,
+                DayOfWeekSet(),
+                MissionType.COUNT,
+                0,
+                0,
+                0,
+                0,
+                3,
+                1,
+                0,
+                0)
+        val history = CompletedHistory(0, mission.id, Calendar.getInstance())
+        val executor = Executors.newSingleThreadExecutor()
+        button1.setOnClickListener {
+            executor.execute {
+                db?.missionDao()?.insertMission(mission)
+            }
 
+        }
+        button2.setOnClickListener {
+            executor.execute {
+                db?.completedHistoryDao()?.insertCompletedHistory(history)
+            }
+        }
     }
 }
