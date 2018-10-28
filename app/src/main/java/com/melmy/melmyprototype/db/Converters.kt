cf546aa552c6950b7cfb30c8f9080878159b8ba5
Sung Ho Hwang <sgc109109@gmail.com>
2018-10-29 02:45:00 +0900

    Entity 에서 사용할 Converter 클래스 생성

---
+++ app/src/main/java/com/melmy/melmyprototype/db/Converters.kt
@@ -0,0 +1,42 @@
+package com.melmy.melmyprototype.db
+
+import androidx.room.TypeConverter
+import com.melmy.melmyprototype.model.MissionType
+import com.melmy.melmyprototype.utils.DayOfWeekSet
+import java.util.*
+
+class Converters {
+    @TypeConverter
+    fun missionTypeToString(missionType: MissionType): String {
+        return when (missionType) {
+            MissionType.COUNT -> "count"
+            MissionType.TIME -> "time"
+        }
+    }
+
+    @TypeConverter
+    fun stringToMissionType(missionType: String): MissionType {
+        return when (missionType) {
+            "count" -> MissionType.COUNT
+            "time" -> MissionType.TIME
+            else -> MissionType.TIME
+        }
+    }
+
+    @TypeConverter
+    fun dayOfWeekSetToBitmask(days: DayOfWeekSet): Int {
+        return days.toInt()
+    }
+
+    @TypeConverter
+    fun bitmaskToDayOfWeekSet(bitmask: Int): DayOfWeekSet {
+        return DayOfWeekSet(bitmask)
+    }
+
+    @TypeConverter
+    fun calendarToTimestamp(calendar: Calendar) = calendar.timeInMillis
+
+    @TypeConverter
+    fun timestampToCalendar(value: Long): Calendar =
+        Calendar.getInstance().apply { timeInMillis = value }
+}
