184463249fadc638052c09c90691216f2247974a
Sung Ho Hwang <sgc109109@gmail.com>
2018-11-10 21:37:54 +0900

    missions 테이블과 JOIN 하여 daily_missions 테이블에서 오늘의 미션 목록 가져오는 쿼리 추가

--- app/src/main/java/com/melmy/melmyprototype/data/DailyMissionDao.kt
+++ app/src/main/java/com/melmy/melmyprototype/data/DailyMissionDao.kt
@@ -5,16 +5,21 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import com.melmy.melmyprototype.model.DailyMission
 
 @Dao
 interface DailyMissionDao {
     @Insert
     fun insertMission(mission: DailyMission)
 
-    @Query("SELECT * FROM daily_missions")
-    fun getAllMissions(): LiveData<List<DailyMission>>
+//    @Query("SELECT * FROM daily_missions")
+//    fun getAllMissions(): LiveData<List<DailyMission>>
 
     @Delete
     fun deleteMission(mission: DailyMission)
+
+    @Query("SELECT daily_missions.*, missions.title, missions.type, missions.goal_minute_daily, missions.goal_count_daily " +
+            "FROM daily_missions " +
+            "INNER JOIN missions " +
+            "ON daily_missions.mission_id = missions.id")
+    fun getAllDailyMissionsJoined(): LiveData<List<DailyMissionJoined>>
 }
